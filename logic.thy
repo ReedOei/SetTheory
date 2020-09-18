7df070546a7a8cb4a59b148646f34d42463be88d
Reed Oei <oei.reed@gmail.com>
2020-09-17 21:17:54 -0500

    ADd some equivalence relation stuff

--- logic.thy
+++ logic.thy
@@ -49,7 +49,7 @@ declare all_def[simp]
 
 definition zipWith :: "('a \<times> 'b \<Rightarrow> 'c) \<Rightarrow> 'a list \<Rightarrow> 'b list \<Rightarrow> 'c list" where
   "zipWith f as bs \<equiv> map f (zip as bs)"
-declare zipWith_def[simp]
+(* declare zipWith_def[simp] *)
 
 definition allRelated :: "'a list \<Rightarrow> ('a \<times> 'b) set \<Rightarrow> 'b list \<Rightarrow> bool" ("_\<sim>\<^sup>__" ) where
   "xs \<sim>\<^sup>E ys \<equiv> (length xs = length ys) \<and> all (zipWith (\<lambda>p. p \<in> E) xs ys)"
@@ -67,45 +67,161 @@ definition func_induced_equiv :: "('b \<Rightarrow> 'c) \<Rightarrow> ('b \<time
 lemma [simp]: "(p \<in> \<sim>\<^sub>h) = (h (fst p) = h (snd p))"
   by (auto simp: func_induced_equiv_def)
 
-lemma nonempty_elem[simp]: "Suc n = length xs \<Longrightarrow> \<exists>a ys. xs = a # ys"
+lemma nonempty_elem: "Suc n = length xs \<Longrightarrow> \<exists>a ys. xs = a # ys"
   by (induction xs, auto)
 
-lemma strong_hom_induced_all_related_is_eq:
-  "\<lbrakk> E = \<sim>\<^sub>h \<rbrakk> \<Longrightarrow> \<forall>l. \<forall>k. (l \<sim>\<^sup>E k) \<longrightarrow> map h l = map h k"
-  apply (rule allI)
-  apply (induct_tac l)
-   apply (simp add: allRelated_def)
-  apply (rule allI)
-  apply (rule impI)
-  apply simp
-  apply (erule_tac x = "tl k" in allE)
-  apply (clarsimp simp: allRelated_def)
-  apply auto
-  by (frule nonempty_elem, erule exE, erule exE, simp)+
-
-lemma [simp]: "\<lbrakk> E = \<sim>\<^sub>h ; l \<sim>\<^sup>E k \<rbrakk> \<Longrightarrow> map h l = map h k"
-  by (drule strong_hom_induced_all_related_is_eq, auto)
-
-(*
-lemma "\<lbrakk> E = \<sim>\<^sub>h ; l \<sim>\<^sup>E k \<rbrakk> \<Longrightarrow> map h l = map h k"
+lemma induced_equiv_map_eq[dest]:
+  fixes l and k and h 
+  assumes all_equiv: "l \<sim>\<^sup>\<sim>\<^sub>h k"
+  shows "map h l = map h k"
+  using all_equiv
 proof(induction l arbitrary: k)
   case Nil
+  then have "k = []" by (auto simp: allRelated_def)
   then show ?case by auto
 next
   case (Cons a l)
-  then have IH: "(Suc (length l) = length k) \<and> (l \<sim>\<^sup>E (tl k)) \<and> map h l = map h (tl k)" by simp
-  then show ?case 
-  proof (cases k)
-    case Nil
-    then have mapKMt: "map h k = []" by simp
-    then have "map h (a # l) = []" 
-  next
-    case (Cons a list)
-    then show ?thesis sorry
+  then have equiv: "(a # l) \<sim>\<^sup>\<sim>\<^sub>h k" 
+        and IH: "\<lbrakk> l \<sim>\<^sup>\<sim>\<^sub>h (tl k) \<rbrakk> \<Longrightarrow> map h l = map h (tl k)" by auto
+
+  then have ex: "\<exists>b j. k = b # j"
+  proof -
+    from equiv have "Suc (length l) = length k" by (simp add: allRelated_def)
+    thus ?thesis by (frule nonempty_elem)
   qed
-qed *)
+
+  then obtain b where "\<exists>j. k = b # j" ..
+  then obtain j where eq: "k = b # j" ..         
+
+  then have tl_eq: "map h l = map h (tl k)"
+  proof -
+    from equiv and eq have "l \<sim>\<^sup>\<sim>\<^sub>h (tl k)" by (simp add: allRelated_def, auto simp: zipWith_def)
+    from this and IH show ?thesis by simp
+  qed
+
+  from equiv and eq and tl_eq show ?case by (simp add: allRelated_def, auto simp: zipWith_def)
+qed
 
 lemma strong_hom_induced_equiv_is_congr: "\<lbrakk> h \<in> strong_hom \<A> \<B> \<rbrakk> \<Longrightarrow> (\<sim>\<^sub>h) \<in> congruence \<A>"
   by (auto simp: strong_hom_def hom_def congruence_def)
+
+definition equiv_class :: "'b \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> 'b set" ("[_]\<^sub>_") where
+  "[x]\<^sub>E \<equiv> {y . (x, y) \<in> E}"
+
+definition bin_rel :: "'b set \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> bool" where
+  "bin_rel A E \<equiv> (\<forall>(x,y) \<in> E. x \<in> A \<and> y \<in> A)"
+
+definition refl_rel :: "'b set \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> bool" where
+  "refl_rel A E \<equiv> bin_rel A E \<and> (\<forall>x. (x,x) \<in> E)"
+
+definition sym_rel :: "'b set \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> bool" where
+  "sym_rel A E \<equiv> bin_rel A E \<and> (\<forall>x y. (x, y) \<in> E \<longleftrightarrow> (y, x) \<in> E)"
+
+definition trans_rel :: "'b set \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> bool" where
+  "trans_rel A E \<equiv> bin_rel A E \<and> (\<forall>x y z. ((x,y) \<in> E \<and> (y, z) \<in> E) \<longrightarrow> (x, z) \<in> E)"
+
+definition equiv_rel :: "'b set \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> bool" where
+  "equiv_rel A E \<equiv> bin_rel A E \<and> refl_rel A E \<and> sym_rel A E \<and> trans_rel A E"
+
+lemma equiv_rel_refl:
+  fixes A and E and x
+  assumes "equiv_rel A E"
+    shows "(x, x) \<in> E"
+  using assms
+  by (simp add: equiv_rel_def refl_rel_def)
+
+lemma equiv_rel_sym:
+  fixes A and E and x and y
+  assumes "equiv_rel A E" and "(x, y) \<in> E"
+    shows "(y, x) \<in> E"
+  using assms
+  by (simp add: equiv_rel_def sym_rel_def)
+
+lemma equiv_rel_trans:
+  fixes A and E and x and y and z
+  assumes "equiv_rel A E" and "(x, y) \<in> E" and "(y, z) \<in> E"
+    shows "(x, z) \<in> E"
+  using assms
+  apply (simp add: equiv_rel_def)
+  apply (unfold trans_rel_def)
+  apply (erule conjE)+
+  apply (erule allE)+
+  by auto
+
+lemma equiv_class_sym:
+  fixes A and E and x and y
+  assumes "equiv_rel A E" and "x \<in> [y]\<^sub>E"
+  shows "(x,y) \<in> E"     
+  using assms
+  apply (auto simp: equiv_class_def)
+  by (erule equiv_rel_sym)
+
+definition quotient_by_rel :: "'b set \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> 'b set set" where
+  "quotient_by_rel A E \<equiv> { C . \<exists>x \<in> A. C = [x]\<^sub>E }"
+
+lemma uniq_eq_class:
+  fixes A and E and C and x
+  assumes eq_rel: "equiv_rel A E"
+      and c: "C \<in> quotient_by_rel A E"
+      and cx: "x \<in> C"
+    shows "C = [x]\<^sub>E"
+proof
+  from eq_rel and c obtain z where z: "C = [z]\<^sub>E" by (auto simp: quotient_by_rel_def)
+  from cx and z have xinz: "x \<in> [z]\<^sub>E" by simp
+  from eq_rel and xinz have xz: "(x, z) \<in> E" by (rule equiv_class_sym)
+
+  show "C \<subseteq> [x]\<^sub>E"
+  proof
+    fix y assume cy: "y \<in> C" show "y \<in> [x]\<^sub>E"
+    proof(simp add: equiv_class_def)  
+      from z and cy have zy: "(z, y) \<in> E" by (simp add: equiv_class_def)
+      from eq_rel and xz and zy show "(x, y) \<in> E" by (rule equiv_rel_trans)
+    qed
+  qed
+
+  show "([x]\<^sub>E) \<subseteq> C"
+  proof
+    fix y assume yinx: "y \<in> [x]\<^sub>E" show "y \<in> C"
+      using z 
+    proof(simp)
+      from eq_rel and yinx have yx: "(y, x) \<in> E" by (rule equiv_class_sym)
+      from eq_rel and yx and xz have "(y, z) \<in> E" by (rule equiv_rel_trans)
+      from eq_rel and this have "(z, y) \<in> E" by (rule equiv_rel_sym)
+      then show "y \<in> [z]\<^sub>E" by (simp add: equiv_class_def)
+    qed
+  qed
+qed
+
+lemma 
+  fixes A and E
+  assumes eq_rel: "equiv_rel A E"
+  shows "\<forall>x \<in> A. \<exists>!C \<in> quotient_by_rel A E. x \<in> C" 
+proof
+  fix x
+  assume xa: "x \<in> A"
+  show "\<exists>!C \<in> quotient_by_rel A E. x \<in> C"
+  proof -
+    let ?C = "[x]\<^sub>E"
+
+    (* For some reason I can't simplify this in any way... *)
+    from xa and eq_rel have "x \<in> ?C \<and> ?C \<in> quotient_by_rel A E"
+      by (auto simp: equiv_class_def quotient_by_rel_def equiv_rel_refl)
+
+    then obtain C where c: "C \<in> quotient_by_rel A E \<and> x \<in> C" by auto
+    then show ?thesis
+    proof(rule ex1I)
+      fix D
+      assume d: "D \<in> quotient_by_rel A E \<and> x \<in> D"
+      show "D = C"
+      proof -
+        from eq_rel and c and d have "D = [x]\<^sub>E" and "C = [x]\<^sub>E" by (auto simp: uniq_eq_class)
+        thus "D = C" by simp
+      qed
+    qed
+  qed
+qed
+
+definition quotient_struct :: "('a, 'b) struct \<Rightarrow> ('b \<times> 'b) set \<Rightarrow> ('a, 'b set) struct" ("_/_") where
+  "\<A> / \<sim> \<equiv> t"
 
 end
