b481e6ca07c1976c73ecfba13111ead186c6fa49
Reed Oei <oei.reed@gmail.com>
2021-08-08 09:19:17 -0500

    Start restructuring and cleaning up code

---
+++ set_lang/src/ast.rs
@@ -0,0 +1,201 @@
+use num_bigint::{BigInt, ToBigUint};
+use std::cell::RefCell;
+use std::collections::{BinaryHeap, HashSet, HashMap, VecDeque};
+use std::fmt;
+use std::fmt::Debug;
+use std::rc::Rc;
+
+use crate::math::{Rat, Sequence};
+
+pub trait BuiltinFunc {
+    fn apply(&self, args : Vec<AST>) -> Result<AST, String>;
+}
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod,
+    Exp,
+    Concat,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+    Equals,
+    NotEquals,
+    Elem,
+    Or,
+    And,
+    Imp,
+    Prove
+}
+
+#[derive(Clone, Educe)]
+#[educe(Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+pub enum AST {
+    Skip(),
+
+    Definition(String, Box<AST>),
+    Rule(Vec<String>, Box<AST>, Box<AST>),
+    Assumption(Box<AST>),
+    Prove(Box<AST>),
+    Counterexample(Box<AST>),
+
+    Int(BigInt),
+    Rat(Rat),
+
+    Var(String),
+
+    Seq(String,
+        #[educe(Debug(ignore))]
+        #[educe(Hash(ignore))]
+        #[educe(Eq(ignore))]
+        #[educe(PartialEq(ignore))]
+        #[educe(Ord(ignore))]
+        #[educe(PartialOrd(ignore))]
+        Rc<RefCell<dyn Sequence>>),
+
+    Builtin(String,
+        #[educe(Debug(ignore))]
+        #[educe(Hash(ignore))]
+        #[educe(Eq(ignore))]
+        #[educe(PartialEq(ignore))]
+        #[educe(Ord(ignore))]
+        #[educe(PartialOrd(ignore))]
+        Rc<RefCell<dyn BuiltinFunc>>),
+
+    FinSet(Vec<AST>),
+    List(Vec<AST>),
+    RangeSet(Box<AST>, Box<AST>, Box<AST>),
+    Image(Box<AST>, Box<AST>),
+    CompSet(Vec<(String, AST)>, Vec<AST>),
+
+    Bin(Op, Box<AST>, Box<AST>),
+
+    App(Box<AST>, Vec<AST>),
+    Function(Option<String>, Vec<String>, Box<AST>),
+    Memo(Option<String>,
+         Vec<String>,
+         Box<AST>,
+         #[educe(Debug(ignore))]
+         #[educe(Hash(ignore))]
+         #[educe(Eq(ignore))]
+         #[educe(PartialEq(ignore))]
+         #[educe(Ord(ignore))]
+         #[educe(PartialOrd(ignore))]
+         Rc<RefCell<HashMap<Vec<AST>, Result<AST, String>>>>
+    ),
+
+    IfThenElse(Box<AST>, Box<AST>, Box<AST>),
+
+    Sum(Box<AST>),
+
+    Factorial(Box<AST>),
+    Negate(Box<AST>),
+    Complement(Box<AST>)
+}
+
+impl Default for AST {
+    fn default() -> AST {
+        AST::Skip()
+    }
+}
+
+impl fmt::Display for AST {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AST::Skip() => write!(f, "skip"),
+            AST::Int(n) => write!(f, "{}", n),
+            AST::Rat(r) => write!(f, "{}", r),
+            AST::Var(x) => write!(f, "{}", x),
+            AST::Definition(name, body) => write!(f, "Let {} := {}", name, body),
+            AST::Rule(attrs, lhs, rhs) => write!(f, "Rule {} => {} {:?}", lhs, rhs, attrs),
+            AST::Assumption(t) => write!(f, "Assume {}", t),
+            AST::Prove(t) => write!(f, "Prove {}", t),
+            AST::Counterexample(t) => write!(f, "Counterexample {}", t),
+            AST::Seq(name, _) => write!(f, "{}", name),
+            AST::Builtin(name, _) => write!(f, "{}", name),
+            AST::FinSet(elems) => {
+                let mut s = String::new();
+                s.push('{');
+                if !elems.is_empty() {
+                    for e in &elems[..elems.len() - 1] {
+                        s.push_str(format!("{}, ", e).as_str());
+                    }
+                    s.push_str(format!("{}", elems[elems.len() - 1]).as_str());
+                }
+                s.push('}');
+                write!(f, "{}", s)
+            }
+            AST::List(elems) => {
+                let mut s = String::new();
+                s.push('[');
+                if !elems.is_empty() {
+                    for e in &elems[..elems.len() - 1] {
+                        s.push_str(format!("{}, ", e).as_str());
+                    }
+                    s.push_str(format!("{}", elems[elems.len() - 1]).as_str());
+                }
+                s.push(']');
+                write!(f, "{}", s)
+            }
+            AST::RangeSet(start, end, step) => write!(f, "{{ {}, {}...{} }}", start, step, end),
+            AST::IfThenElse(cond, then_expr, else_expr) => write!(f, "(if {} then {} else {})", cond, then_expr, else_expr),
+            AST::Image(a, b) => write!(f, "{}[{}]", a, b),
+
+            AST::Bin(Op::Add, a, b) => write!(f, "({} + {})", a, b),
+            AST::Bin(Op::Sub, a, b) => write!(f, "({} - {})", a, b),
+            AST::Bin(Op::Mul, a, b) => write!(f, "({} * {})", a, b),
+            AST::Bin(Op::Div, a, b) => write!(f, "({} / {})", a, b),
+            AST::Bin(Op::Mod, a, b) => write!(f, "({} % {})", a, b),
+            AST::Bin(Op::Exp, a, b) => write!(f, "({}^{})", a, b),
+
+            AST::Bin(Op::Concat, a, b) => write!(f, "({} @ {})", a, b),
+
+            AST::Bin(Op::And, a, b) => write!(f, "({} and {})", a, b),
+            AST::Bin(Op::Or, a, b) => write!(f, "({} or {})", a, b),
+            AST::Bin(Op::Imp, a, b) => write!(f, "({} ==> {})", a, b),
+
+            AST::Bin(Op::Elem, a, b) => write!(f, "({} ∈ {})", a, b),
+
+            AST::Bin(Op::Prove, a, b) => write!(f, "({} |- {})", a, b),
+
+            AST::Bin(Op::Lt, a, b) => write!(f, "({} < {})", a, b),
+            AST::Bin(Op::Le, a, b) => write!(f, "({} ≤ {})", a, b),
+            AST::Bin(Op::Gt, a, b) => write!(f, "({} > {})", a, b),
+            AST::Bin(Op::Ge, a, b) => write!(f, "({} ≥ {})", a, b),
+            AST::Bin(Op::Equals, a, b) => write!(f, "({} = {})", a, b),
+            AST::Bin(Op::NotEquals, a, b) => write!(f, "({} ≠ {})", a, b),
+
+            AST::CompSet(var_doms, clauses) => {
+                let mut s = String::new();
+                s.push('{');
+                s.push_str(var_doms.iter().map(|(x, dom)| format!("{} ∈ {}", x, dom)).collect::<Vec<String>>().join(", ").as_str());
+                s.push_str(" : ");
+                s.push_str(clauses.iter().map(|clause| format!("{}", clause)).collect::<Vec<String>>().join(", ").as_str());
+                s.push('}');
+                write!(f, "{}", s)
+            }
+
+            AST::Memo(name, args, body, _) => write!(f, "memo({:?}: {:?} |-> {})", name, args, body),
+            AST::Function(name, args, body) => write!(f, "{:?}: {:?} |-> {}", name, args, body),
+            AST::App(name, xs) => {
+                if **name == AST::Var("|".to_string()) {
+                    write!(f, "({} | {})", xs[0], xs[1])
+                } else {
+                    write!(f, "{}({})", name, xs.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(", "))
+                }
+            }
+
+            AST::Sum(a) => write!(f, "(Σ {})", a),
+            AST::Negate(a) => write!(f, "(- {})", a),
+            AST::Factorial(a) => write!(f, "({}!)", a),
+            AST::Complement(a) => write!(f, "(not {})", a),
+        }
+    }
+}
+
+
--- set_lang/src/main.rs
+++ set_lang/src/main.rs
@@ -21,13 +21,16 @@ use std::collections::{BinaryHeap, HashSet, HashMap, VecDeque};
 use std::cmp::{max, Ordering, Reverse};
 use std::env;
 use std::fs;
-use std::fmt;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::iter::Iterator;
-use std::ops;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::rc::Rc;
+
+mod math;
+use math::*;
+mod ast;
+use ast::*;
 
 static mut FRESH_COUNTER : usize = 0;
 
@@ -41,19 +44,6 @@ pub fn to_usize(n : &BigInt) -> Result<usize, String> {
             .map(|d| d as usize)
             .fold(0, |accum, d| accum * (std::u64::MAX as usize) + d)),
         None => Err(format!("Could not convert {:?} to usize", n))
-    }
-}
-
-pub trait Sequence {
-    fn nth(&mut self, n : usize) -> Result<AST, String>;
-    fn increasing(&self) -> bool;
-    fn index_of(&mut self, v : AST) -> Option<usize>;
-}
-
-pub struct Naturals;
-impl Naturals {
-    fn new() -> Naturals {
-        return Naturals;
     }
 }
 
@@ -137,178 +127,6 @@ pub fn prime_factor(n_in : BigInt, ps : &mut PrimeSeq) -> std::collections::hash
     }
     *powers.entry(n).or_insert(Zero::zero()) += 1;
     return powers.into_iter();
-}
-
-#[derive(Debug, Clone)]
-pub struct Rat {
-    n : BigInt,
-    d : BigInt
-}
-
-impl PartialEq for Rat {
-    fn eq(&self, other : &Rat) -> bool {
-        return self.n.clone() * other.d.clone() == other.n.clone() * self.d.clone();
-    }
-}
-
-impl PartialOrd for Rat {
-    fn partial_cmp(&self, other : &Rat) -> Option<std::cmp::Ordering> {
-        return (self.n.clone() * other.d.clone()).partial_cmp(&(other.n.clone() * self.d.clone()));
-    }
-}
-
-impl Eq for Rat {
-
-}
-
-impl Ord for Rat {
-    fn cmp(&self, other: &Rat) -> std::cmp::Ordering {
-        return (self.n.clone() * other.d.clone()).cmp(&(other.n.clone() * self.d.clone()));
-    }
-}
-
-impl Hash for Rat {
-    fn hash<H>(&self, state : &mut H) where H: std::hash::Hasher {
-        let r = self.clone().simplify();
-        r.n.hash(state);
-        r.d.hash(state);
-    }
-}
-
-impl fmt::Display for Rat {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}/{}", self.n, self.d)
-    }
-}
-
-pub fn gcd(a : BigInt, b : BigInt) -> BigInt {
-    let mut x = a;
-    let mut y = b;
-    while y != Zero::zero() {
-        let temp = y.clone();
-        y = x % y;
-        x = temp;
-    }
-    return x;
-}
-
-impl Rat {
-    pub fn new(n : BigInt, d : BigInt) -> Rat {
-        let r = Rat { n, d };
-        return r.simplify();
-    }
-
-    pub fn from_usize(n : usize) -> Rat {
-        return Rat::new(BigInt::from(n), One::one());
-    }
-
-    pub fn simplify(mut self) -> Rat {
-        let g = gcd(self.n.clone(), self.d.clone());
-        self.n /= g.clone();
-        self.d /= g;
-        if self.d < Zero::zero() && self.n < Zero::zero() {
-            std::mem::swap(&mut self.n, &mut self.d);
-        }
-        return self;
-    }
-
-    pub fn pow(mut self, a : &BigInt) -> Rat {
-        if a > &Zero::zero() {
-            let mut n : BigInt = One::one();
-            let orig_n = self.n.clone();
-            let orig_d = self.d.clone();
-            while &n < a {
-                self.n *= orig_n.clone();
-                self.d *= orig_d.clone();
-                n += 1;
-            }
-            return self;
-        } else if a < &Zero::zero() {
-            std::mem::swap(&mut self.n, &mut self.d);
-            return self.pow(&-a);
-        } else {
-            return Rat { n: One::one(), d: One::one() };
-        }
-    }
-}
-
-impl ops::Add<BigInt> for Rat {
-    type Output = Rat;
-
-    fn add(self, b : BigInt) -> Rat {
-        return Rat::new(self.n + b * self.d.clone(), self.d);
-    }
-}
-
-impl ops::Sub<BigInt> for Rat {
-    type Output = Rat;
-
-    fn sub(self, b : BigInt) -> Rat {
-        return Rat::new(self.n - b * self.d.clone(), self.d);
-    }
-}
-
-impl ops::Mul<BigInt> for Rat {
-    type Output = Rat;
-
-    fn mul(self, b : BigInt) -> Rat {
-        return Rat::new(self.n * b, self.d);
-    }
-}
-
-impl ops::Div<BigInt> for Rat {
-    type Output = Rat;
-
-    fn div(self, b : BigInt) -> Rat {
-        return Rat::new(self.n, self.d * b);
-    }
-}
-
-impl ops::Add<Rat> for Rat {
-    type Output = Rat;
-
-    fn add(self, b : Rat) -> Rat {
-        return Rat::new(self.n * b.d.clone() + b.n * self.d.clone(), self.d * b.d);
-    }
-}
-
-impl ops::Sub<Rat> for Rat {
-    type Output = Rat;
-
-    fn sub(self, b : Rat) -> Rat {
-        return Rat::new(self.n * b.d.clone() - b.n * self.d.clone(), self.d * b.d);
-    }
-}
-
-impl ops::Mul<Rat> for Rat {
-    type Output = Rat;
-
-    fn mul(self, b : Rat) -> Rat {
-        return Rat::new(self.n * b.n, self.d * b.d);
-    }
-}
-
-impl ops::Div<Rat> for Rat {
-    type Output = Rat;
-
-    fn div(self, b : Rat) -> Rat {
-        return Rat::new(self.n * b.d, self.d * b.n);
-    }
-}
-
-impl ops::MulAssign<Rat> for Rat {
-    fn mul_assign(&mut self, b : Rat) {
-        self.n *= b.n;
-        self.d *= b.d;
-    }
-}
-
-impl ops::Neg for Rat {
-    type Output = Rat;
-
-    fn neg(self) -> Rat {
-        return Rat { n: -self.n, d: self.d };
-    }
 }
 
 pub struct Rationals {
@@ -604,203 +422,12 @@ impl Sequence for PrimeSeq {
     }
 }
 
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub enum Op {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Mod,
-    Exp,
-    Concat,
-    Lt,
-    Le,
-    Gt,
-    Ge,
-    Equals,
-    NotEquals,
-    Elem,
-    Or,
-    And,
-    Imp,
-    Prove
-}
-
-pub trait BuiltinFunc {
-    fn apply(&self, args : Vec<AST>) -> Result<AST, String>;
-}
-
-#[derive(Clone, Educe)]
-#[educe(Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub enum AST {
-    Skip(),
-
-    Definition(String, Box<AST>),
-    Rule(Vec<String>, Box<AST>, Box<AST>),
-    Assumption(Box<AST>),
-    Prove(Box<AST>),
-    Counterexample(Box<AST>),
-
-    Int(BigInt),
-    Rat(Rat),
-
-    Var(String),
-
-    Seq(String,
-        #[educe(Debug(ignore))]
-        #[educe(Hash(ignore))]
-        #[educe(Eq(ignore))]
-        #[educe(PartialEq(ignore))]
-        #[educe(Ord(ignore))]
-        #[educe(PartialOrd(ignore))]
-        Rc<RefCell<dyn Sequence>>),
-
-    Builtin(String,
-        #[educe(Debug(ignore))]
-        #[educe(Hash(ignore))]
-        #[educe(Eq(ignore))]
-        #[educe(PartialEq(ignore))]
-        #[educe(Ord(ignore))]
-        #[educe(PartialOrd(ignore))]
-        Rc<RefCell<dyn BuiltinFunc>>),
-
-    FinSet(Vec<AST>),
-    List(Vec<AST>),
-    RangeSet(Box<AST>, Box<AST>, Box<AST>),
-    Image(Box<AST>, Box<AST>),
-    CompSet(Vec<(String, AST)>, Vec<AST>),
-
-    Bin(Op, Box<AST>, Box<AST>),
-
-    App(Box<AST>, Vec<AST>),
-    Function(Option<String>, Vec<String>, Box<AST>),
-    Memo(Option<String>,
-         Vec<String>,
-         Box<AST>,
-         #[educe(Debug(ignore))]
-         #[educe(Hash(ignore))]
-         #[educe(Eq(ignore))]
-         #[educe(PartialEq(ignore))]
-         #[educe(Ord(ignore))]
-         #[educe(PartialOrd(ignore))]
-         Rc<RefCell<HashMap<Vec<AST>, Result<AST, String>>>>
-    ),
-
-    IfThenElse(Box<AST>, Box<AST>, Box<AST>),
-
-    Sum(Box<AST>),
-
-    Factorial(Box<AST>),
-    Negate(Box<AST>),
-    Complement(Box<AST>)
-}
-
-impl Default for AST {
-    fn default() -> AST {
-        AST::Skip()
-    }
-}
-
 fn format_assignment(a : &HashMap<String, AST>) -> String {
     let mut s = String::new();
     s.push('{');
     s.push_str(a.iter().map(|(k, v)| format!("{} -> {}", k, v)).collect::<Vec<String>>().join(", ").as_str());
     s.push('}');
     return s;
-}
-
-impl fmt::Display for AST {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            AST::Skip() => write!(f, "skip"),
-            AST::Int(n) => write!(f, "{}", n),
-            AST::Rat(r) => write!(f, "{}", r),
-            AST::Var(x) => write!(f, "{}", x),
-            AST::Definition(name, body) => write!(f, "Let {} := {}", name, body),
-            AST::Rule(attrs, lhs, rhs) => write!(f, "Rule {} => {} {:?}", lhs, rhs, attrs),
-            AST::Assumption(t) => write!(f, "Assume {}", t),
-            AST::Prove(t) => write!(f, "Prove {}", t),
-            AST::Counterexample(t) => write!(f, "Counterexample {}", t),
-            AST::Seq(name, _) => write!(f, "{}", name),
-            AST::Builtin(name, _) => write!(f, "{}", name),
-            AST::FinSet(elems) => {
-                let mut s = String::new();
-                s.push('{');
-                if !elems.is_empty() {
-                    for e in &elems[..elems.len() - 1] {
-                        s.push_str(format!("{}, ", e).as_str());
-                    }
-                    s.push_str(format!("{}", elems[elems.len() - 1]).as_str());
-                }
-                s.push('}');
-                write!(f, "{}", s)
-            }
-            AST::List(elems) => {
-                let mut s = String::new();
-                s.push('[');
-                if !elems.is_empty() {
-                    for e in &elems[..elems.len() - 1] {
-                        s.push_str(format!("{}, ", e).as_str());
-                    }
-                    s.push_str(format!("{}", elems[elems.len() - 1]).as_str());
-                }
-                s.push(']');
-                write!(f, "{}", s)
-            }
-            AST::RangeSet(start, end, step) => write!(f, "{{ {}, {}...{} }}", start, step, end),
-            AST::IfThenElse(cond, then_expr, else_expr) => write!(f, "(if {} then {} else {})", cond, then_expr, else_expr),
-            AST::Image(a, b) => write!(f, "{}[{}]", a, b),
-
-            AST::Bin(Op::Add, a, b) => write!(f, "({} + {})", a, b),
-            AST::Bin(Op::Sub, a, b) => write!(f, "({} - {})", a, b),
-            AST::Bin(Op::Mul, a, b) => write!(f, "({} * {})", a, b),
-            AST::Bin(Op::Div, a, b) => write!(f, "({} / {})", a, b),
-            AST::Bin(Op::Mod, a, b) => write!(f, "({} % {})", a, b),
-            AST::Bin(Op::Exp, a, b) => write!(f, "({}^{})", a, b),
-
-            AST::Bin(Op::Concat, a, b) => write!(f, "({} @ {})", a, b),
-
-            AST::Bin(Op::And, a, b) => write!(f, "({} and {})", a, b),
-            AST::Bin(Op::Or, a, b) => write!(f, "({} or {})", a, b),
-            AST::Bin(Op::Imp, a, b) => write!(f, "({} ==> {})", a, b),
-
-            AST::Bin(Op::Elem, a, b) => write!(f, "({} ∈ {})", a, b),
-
-            AST::Bin(Op::Prove, a, b) => write!(f, "({} |- {})", a, b),
-
-            AST::Bin(Op::Lt, a, b) => write!(f, "({} < {})", a, b),
-            AST::Bin(Op::Le, a, b) => write!(f, "({} ≤ {})", a, b),
-            AST::Bin(Op::Gt, a, b) => write!(f, "({} > {})", a, b),
-            AST::Bin(Op::Ge, a, b) => write!(f, "({} ≥ {})", a, b),
-            AST::Bin(Op::Equals, a, b) => write!(f, "({} = {})", a, b),
-            AST::Bin(Op::NotEquals, a, b) => write!(f, "({} ≠ {})", a, b),
-
-            AST::CompSet(var_doms, clauses) => {
-                let mut s = String::new();
-                s.push('{');
-                s.push_str(var_doms.iter().map(|(x, dom)| format!("{} ∈ {}", x, dom)).collect::<Vec<String>>().join(", ").as_str());
-                s.push_str(" : ");
-                s.push_str(clauses.iter().map(|clause| format!("{}", clause)).collect::<Vec<String>>().join(", ").as_str());
-                s.push('}');
-                write!(f, "{}", s)
-            }
-
-            AST::Memo(name, args, body, _) => write!(f, "memo({:?}: {:?} |-> {})", name, args, body),
-            AST::Function(name, args, body) => write!(f, "{:?}: {:?} |-> {}", name, args, body),
-            AST::App(name, xs) => {
-                if **name == AST::Var("|".to_string()) {
-                    write!(f, "({} | {})", xs[0], xs[1])
-                } else {
-                    write!(f, "{}({})", name, xs.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(", "))
-                }
-            }
-
-            AST::Sum(a) => write!(f, "(Σ {})", a),
-            AST::Negate(a) => write!(f, "(- {})", a),
-            AST::Factorial(a) => write!(f, "({}!)", a),
-            AST::Complement(a) => write!(f, "(not {})", a),
-        }
-    }
 }
 
 pub fn to_ast(pair : Pair<Rule>, arith_climber : &PrecClimber<Rule>, bool_climber : &PrecClimber<Rule>) -> Result<AST, String> {
